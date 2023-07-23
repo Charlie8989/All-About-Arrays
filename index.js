18031c86878fcdcfefc960d64da399537efc8a72
Charlie8989 <134526994+Charlie8989@users.noreply.github.com>
2023-07-23 22:51:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//let arr=[30,38,57,40]
+//let b=arr.toString()
+//console.log(b)---->prints array without square brackett
+//let c=[28,39,76]
+//console.log(arr.concat(c))--->join arr and c
+//console.log(arr.push(31,39,57))--->prints length of array after adding 3 more
+//console.log(arr.pop())--->prints the last element of array
+//let b=arr.shift()
+//console.log(arr)---->removes starter element
+//let b=arr.unshift(88)
+//console.log(arr)---->add 88 to start
+//let b=arr.slice(1,3)
+//console.log(b)----->consider it as [1,3) it will print 1st and 2nd element
+//let b=arr.splice(1,2,1001,102)
+//console.log(arr)----->prints 1001 and 102 at 1st and 2nd position
+//console.log(arr.reverse())---->reverse the array
+//console.log(arr.sort())----->it will reorder your array
+//let compare=(a,b)=>{
+//  return b-a;
+//}
+//console.log(arr.sort(compare))--->prints array in descending order
+//let compare=(a,b)=>{
+//  return a-b;
+//}
+//console.log(arr.sort(compare))---->prints an array in ascending order
+//let b=arr.map((value,index,array)=>{
+ // return value*value;
+//})
+//console.log(b)---->prints an arrray with the value*value operation
+//let b=arr.filter((c)=>{
+//  return c<50
+//})
+//console.log(b)---->prints an array less than 50
+//let add=(c,d)=>{
+ // return c+d;
+//}
+//let sum=arr.reduce(add)
+//console.log(sum)---->add all element of array
+//let b=Array.from("CHARLIE")--->jo brackett ke andar hai whi print hoga bas 
+//console.log(b)---->it prints charlie in form of array
